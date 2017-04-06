03090ddef12e099a75a0002464a8cd984a516653
andrewrobot <andrew.ed.roberts@gmail.com>
2017-04-06 14:56:17 -0400

    shp2geo testing

--- Script/shp2geojson.js-master/preview.js
+++ Script/shp2geojson.js-master/preview.js
@@ -36,11 +36,16 @@ function loadshp(config, returnData) {
         if(typeof url != 'string') {
             var reader = new FileReader();
             reader.onload = function(e) {
+                try {
                 var URL = window.URL || window.webkitURL || window.mozURL || window.msURL,
                 zip = new JSZip(e.target.result),
                 shpString =  zip.file(/.shp$/i)[0].name,
                 dbfString = zip.file(/.dbf$/i)[0].name,
                 prjString = zip.file(/.prj$/i)[0];
+                } catch(err) {
+                    console.log(err);
+                    return(err);
+                }
                 if(prjString) {
                     proj4.defs('EPSGUSER', zip.file(prjString.name).asText());
                     try {
